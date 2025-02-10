306375545d02709bad08769612e7a907886d30d0
somythl <samuel.mahjouri@gmail.com>
2025-02-09 22:37:45 -0800

    installed elocuent

--- global.js
+++ global.js
@@ -23,6 +23,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'https://github.com/somythl/', title: 'GitHub' },
 ];
 
